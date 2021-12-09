0f286eeef9cdc69afd332e39681662fb67e7a369
dalarcont <dalarcont@DALARCONT>
2021-12-09 01:17:02 -0500

    Agregar archivos de proyecto.

---
+++ DA_Unidad2/DA_Unidad2/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DA_Unidad2.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DA_Unidad2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private async void btnStartLogin(object sender, EventArgs e)
+        {
+            await DisplayAlert("RankStudent", "Ingreso fallido para el usuario:\n"+RS_Login_User.Text, "Cancelar");
+        }
+    }
+}
---
+++ DA_Unidad2/DA_Unidad2/Style.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DA_Unidad2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Style : ResourceDictionary
+    {
+        public Style()
+        {
+
+        }
+    }
+}
